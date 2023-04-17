94bec21548a0af18b2927ccb0236f175c3fb57dc
rising-star92 <ryan.service.1001@gmail.com>
2023-04-17 16:16:51 +0700

    chore: update api documentation

--- selleraxis/files/views.py
+++ selleraxis/files/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import serializers, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -9,6 +11,16 @@ from selleraxis.files.services import get_multi_presigned_url
 class GetUploadPresignedURLView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "amount",
+                openapi.IN_QUERY,
+                description="Amount presigned URL",
+                type=openapi.TYPE_NUMBER,
+            )
+        ]
+    )
     def get(self, _):
         amount = self.request.query_params.get("amount", default="1")
         if not amount.isdigit():
