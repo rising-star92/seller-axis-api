40500daca9a6a82bc8c9d35ec502b7168c5b940c
rising-star92 <ryan.service.1001@gmail.com>
2023-07-06 14:09:37 +0700

    chore: update organization model

--- selleraxis/organizations/models.py
+++ selleraxis/organizations/models.py
@@ -5,6 +5,13 @@ from selleraxis.users.models import User
 
 class Organization(models.Model):
     name = models.CharField(max_length=255)
+    avatar = models.TextField(blank=True, default="")
+    description = models.TextField(blank=True, default="")
+    timezone = models.CharField(blank=True, default="", max_length=128)
+    address = models.CharField(blank=True, default="", max_length=255)
+    email = models.CharField(blank=True, default="", max_length=255)
+    phone = models.CharField(blank=True, default="", max_length=128)
+    status = models.CharField(blank=True, default="", max_length=255)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
