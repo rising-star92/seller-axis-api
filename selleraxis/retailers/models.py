add6098909e7af7235ca7f3ea91c720ee80d02cf
rising-star92 <ryan.service.1001@gmail.com>
2023-07-06 15:14:34 +0700

    chore: change model of retailer

--- selleraxis/retailers/models.py
+++ selleraxis/retailers/models.py
@@ -5,16 +5,7 @@ from selleraxis.organizations.models import Organization
 
 class Retailer(models.Model):
     name = models.CharField(max_length=255)
-    sftp_host = models.CharField(max_length=255)
-    sftp_username = models.CharField(max_length=255)
-    sftp_password = models.CharField(max_length=255)
-    purchase_orders_sftp_directory = models.CharField(max_length=255)
-    acknowledgment_sftp_directory = models.CharField(max_length=255)
-    confirm_sftp_directory = models.CharField(max_length=255)
-    inventory_sftp_directory = models.CharField(max_length=255)
-    invoice_sftp_directory = models.CharField(max_length=255)
-    return_sftp_directory = models.CharField(max_length=255)
-    payment_sftp_directory = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, blank=True, default="")
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- selleraxis/retailers/serializers.py
+++ selleraxis/retailers/serializers.py
@@ -1,14 +1,9 @@
 from rest_framework import serializers
 
 from selleraxis.retailers.models import Retailer
-from selleraxis.retailers.services.check_sftp import check_sftp
 
 
 class RetailerSerializer(serializers.ModelSerializer):
-    def validate(self, data):
-        check_sftp(data)
-        return data
-
     class Meta:
         model = Retailer
         fields = "__all__"
