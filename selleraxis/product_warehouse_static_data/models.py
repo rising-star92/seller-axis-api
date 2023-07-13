44bb508af590766262c5231b963a3bf7eb541d0d
rising-star92 <ryan.service.1001@gmail.com>
2023-07-14 00:56:27 +0700

    chore: product warehouse static data

--- selleraxis/product_warehouse_static_data/models.py
+++ selleraxis/product_warehouse_static_data/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 from selleraxis.retailer_warehouse_products.models import RetailerWarehouseProduct
@@ -6,8 +8,8 @@ from selleraxis.retailer_warehouse_products.models import RetailerWarehouseProdu
 class ProductWarehouseStaticData(models.Model):
     status = models.CharField(max_length=255)
     qty_on_hand = models.IntegerField()
-    next_available_qty = models.IntegerField()
-    next_available_date = models.DateTimeField()
+    next_available_qty = models.IntegerField(blank=True, default=0)
+    next_available_date = models.DateTimeField(blank=True, default=datetime.now)
     product_warehouse_id = models.ForeignKey(
         RetailerWarehouseProduct,
         on_delete=models.CASCADE,
