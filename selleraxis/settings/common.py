a99b56815219798343310e6cc227b8e5b32ac11c
rising-star92 <ryan.service.1001@gmail.com>
2023-07-20 14:00:15 +0700

    chore: Change access token lifetime to 60 second

--- selleraxis/settings/common.py
+++ selleraxis/settings/common.py
@@ -155,7 +155,7 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 # JWT config
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
