4a6111abb4d49edfb2cdba5816376264c94250a0
rising-star92 <ryan.service.1001@gmail.com>
2023-07-21 14:22:13 +0700

    chore: show product seres

--- selleraxis/package_rules/models.py
+++ selleraxis/package_rules/models.py
@@ -7,7 +7,7 @@ from selleraxis.product_series.models import ProductSeries
 class PackageRule(models.Model):
     max_quantity = models.IntegerField(default=1)
     product_series = models.ForeignKey(
-        ProductSeries, on_delete=models.CASCADE, null=True
+        ProductSeries, on_delete=models.CASCADE, null=True, related_name="package_rules"
     )
     box = models.ForeignKey(Box, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
