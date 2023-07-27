f5fcb9442807cbfc6bfa2087e343c56cf061464b
rising-star92 <ryan.service.1001@gmail.com>
2023-07-27 11:18:24 +0700

    chore: update model retailer

--- selleraxis/retailers/models.py
+++ selleraxis/retailers/models.py
@@ -6,6 +6,7 @@ from selleraxis.organizations.models import Organization
 class Retailer(models.Model):
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255, blank=True, default="")
+    merchant_id = models.CharField(max_length=255, default="lowes")
     organization = models.ForeignKey(
         Organization, on_delete=models.CASCADE, related_name="retailer_organization"
     )
