d9c91e99a1fb9bcc7ff36cefa2191ab078cb26a4
rising-star92 <ryan.service.1001@gmail.com>
2023-08-07 16:42:11 +0700

    fix: fix error create order item package

--- selleraxis/order_item_package/services.py
+++ selleraxis/order_item_package/services.py
@@ -33,8 +33,8 @@ def create_order_item_package_service(package, order_item, quantity):
         box_limit = package_rule.max_quantity
         for order_item_package in list_ord_item_package:
             check_qty_order += order_item_package.quantity
-            if order_item.package.box.id == package.box.id:
-                box_limit = box_limit - order_item.quantity * product_alias.sku_quantity
+            if order_item_package.package.box.id == package.box.id:
+                box_limit = box_limit - order_item_package.quantity * product_alias.sku_quantity
         if box_limit < quantity * product_alias.sku_quantity:
             return {
                 "status": 400,
