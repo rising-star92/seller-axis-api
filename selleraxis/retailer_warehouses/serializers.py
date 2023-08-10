e3730ff2687cee2bac1c6ff697099142aa3e8775
rising-star92 <ryan.service.1001@gmail.com>
2023-08-10 09:45:42 +0700

    [BE] Update API for address at Retailer Warehouse

--- selleraxis/retailer_warehouses/serializers.py
+++ selleraxis/retailer_warehouses/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework import exceptions, serializers
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from selleraxis.retailer_warehouses.models import RetailerWarehouse
 from selleraxis.retailers.models import Retailer
@@ -6,10 +7,23 @@ from selleraxis.retailers.models import Retailer
 
 class RetailerWarehouseAliasSerializer(serializers.ModelSerializer):
     def validate(self, data):
-        if "retailer" in data and self.context["view"].request.headers.get(
-            "organization", None
-        ) != str(data["retailer"].organization.id):
-            raise exceptions.ParseError("Retailer rule must is of organization")
+        if "address_1" in data and not data["address_1"]:
+            raise ValidationError(detail={"address_1": ["Address field is required"]})
+        if "city" in data and not data["city"]:
+            raise ValidationError({"city": ["City field is required"]})
+        if "state" in data and not data["state"]:
+            raise ValidationError({"state": ["State field is required"]})
+        if "postal_code" in data and not data["postal_code"]:
+            raise ValidationError({"postal_code": ["Postal code field is required"]})
+        if "country" in data:
+            if not data["country"]:
+                raise ValidationError({"country": ["Country field is required"]})
+            if len(data["country"]) != 2:
+                raise ValidationError(
+                    {"country": ["Country length field is required 2 letters"]}
+                )
+        if "phone" in data and not data["phone"]:
+            raise ValidationError({"phone": ["Phone field is required"]})
         return data
 
     class Meta:
@@ -35,8 +49,6 @@ class RetailerShowSerializer(serializers.ModelSerializer):
 
 
 class ReadRetailerWarehouseSerializer(serializers.ModelSerializer):
-    retailer = RetailerShowSerializer(read_only=True)
-
     class Meta:
         model = RetailerWarehouse
         fields = "__all__"
