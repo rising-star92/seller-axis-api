e2af7a4bae5d365f85fd17d60ebbbde0f4ae29f1
rising-star92 <ryan.service.1001@gmail.com>
2023-08-14 17:54:02 +0700

    RM-3867: [BE] Validation for Merchant SKU

---
+++ selleraxis/retailer_suggestion/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RetailerSuggestionConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "selleraxis.retailer_suggestion"
