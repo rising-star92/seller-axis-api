6cc9f616d6109cf4de20aac1caf1481f688f719e
rising-star92 <ryan.service.1001@gmail.com>
2023-08-17 16:44:41 +0700

    fix: organzation

--- selleraxis/organizations/serializers.py
+++ selleraxis/organizations/serializers.py
@@ -5,7 +5,7 @@ from selleraxis.roles.serializers import RoleSerializer
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
-    user_roles = serializers.SerializerMethodField()
+    user_roles = RoleSerializer(many=True, read_only=True)
     gs1 = serializers.CharField(max_length=255, required=True)
 
     def get_user_roles(self, organization):
