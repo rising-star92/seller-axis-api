02905f72453ecf735f8176e2e0eb739c7155ee85
rising-star92 <ryan.service.1001@gmail.com>
2023-09-06 14:45:20 +0700

    chore: update ship from

--- selleraxis/addresses/views.py
+++ selleraxis/addresses/views.py
@@ -1,12 +1,19 @@
+from rest_framework import status
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from selleraxis.addresses.models import Address
-from selleraxis.addresses.serializers import AddressSerializer, ReadAddressSerializer
+from selleraxis.addresses.serializers import (
+    AddressSerializer,
+    CreateAddressSerializer,
+    ReadAddressSerializer,
+)
 from selleraxis.core.pagination import Pagination
 from selleraxis.core.permissions import check_permission
 from selleraxis.permissions.models import Permissions
+from selleraxis.retailers.models import Retailer
 
 
 class ListCreateAddressView(ListCreateAPIView):
@@ -22,7 +29,21 @@ class ListCreateAddressView(ListCreateAPIView):
     def get_serializer_class(self):
         if self.request.method == "GET":
             return ReadAddressSerializer
-        return AddressSerializer
+        return CreateAddressSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer_data = serializer.data
+        retailer_id = serializer_data.pop("retailer_id", None)
+        address_data = AddressSerializer(data=serializer_data)
+        address_data.is_valid(raise_exception=True)
+        address_data.save()
+        if retailer_id:
+            Retailer.objects.filter(id=retailer_id).update(
+                ship_from_address_id=address_data.data["id"]
+            )
+        return Response(address_data.data, status=status.HTTP_201_CREATED)
 
     def check_permissions(self, _):
         match self.request.method:
