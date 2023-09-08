201f75b9fe94b2077aec40ee8dabc6428b56f00c
rising-star92 <ryan.service.1001@gmail.com>
2023-09-08 10:17:17 +0700

    4359 - fix create ship from

--- selleraxis/retailers/serializers.py
+++ selleraxis/retailers/serializers.py
@@ -180,6 +180,7 @@ class CreateAddressSerializer(serializers.ModelSerializer):
         fields = "__all__"
         extra_kwargs = {
             "id": {"read_only": True},
+            "verified_carrier": {"read_only": True},
             "organization": {"read_only": True},
             "created_at": {"read_only": True},
             "updated_at": {"read_only": True},
