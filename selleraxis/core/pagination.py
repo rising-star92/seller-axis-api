461ce6875b2c688b459ff02ccb76b3d0b69f104f
rising-star92 <ryan.service.1001@gmail.com>
2023-09-13 16:11:31 +0700

    4525 - feat: implement Row per page for all data in list

--- selleraxis/core/pagination.py
+++ selleraxis/core/pagination.py
@@ -3,3 +3,8 @@ from rest_framework.pagination import LimitOffsetPagination
 
 class Pagination(LimitOffsetPagination):
     default_limit = 10
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if request.query_params.get("limit") == "-1":
+            return None
+        return super().paginate_queryset(queryset, request, view)
