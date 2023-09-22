6c1c93eb34e5a8d54f6d3d30cf3fa5d3481e8157
rising-star92 <ryan.service.1001@gmail.com>
2023-09-22 10:16:35 +0700

    4600 - feat:  Implement API for bulk delete prod alias and prod

--- selleraxis/products/views.py
+++ selleraxis/products/views.py
@@ -1,6 +1,14 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    GenericAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from selleraxis.core.pagination import Pagination
 from selleraxis.core.permissions import check_permission
@@ -61,3 +69,29 @@ class UpdateDeleteProductView(RetrieveUpdateDestroyAPIView):
                 return check_permission(self, Permissions.DELETE_PRODUCT)
             case _:
                 return check_permission(self, Permissions.UPDATE_PRODUCT)
+
+
+class BulkDeleteProductView(GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "ids",
+                openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+            )
+        ]
+    )
+    def delete(self, request, *args, **kwargs):
+        ids = request.query_params.get("ids")
+        organization_id = self.request.headers.get("organization")
+        if ids:
+            list_id = ids.split(",")
+            Product.objects.filter(
+                id__in=list_id, product_series__organization_id=organization_id
+            ).delete()
+        return Response(
+            data={"data": "Products deleted successfully"},
+            status=status.HTTP_200_OK,
+        )
