906551bb19992e33b984556cb09eabfa205f4518
rising-star92 <ryan.service.1001@gmail.com>
2023-12-01 17:24:17 +0700

    feat: try catch to get order

--- selleraxis/getting_order_histories/services.py
+++ selleraxis/getting_order_histories/services.py
@@ -10,18 +10,21 @@ NEXT_EXCUTION_TIME_CACHE = "next_excution_{}"
 
 
 def get_next_execution_time(organization):
-    cache_key = NEXT_EXCUTION_TIME_CACHE.format(organization)
-    cache_response = cache.get(cache_key)
-    if cache_response and cache_response > datetime.now(tz=pytz.utc):
-        return cache_response
-    rule_name = settings.GETTING_NEW_ORDER_RULE_NAME
-    rule_details = client_events.describe_rule(Name=rule_name)
-    schedule_expression = rule_details["ScheduleExpression"]
-    schedule_expression = (
-        schedule_expression.replace("cron(", "").replace(")", "").replace("?", "")
-    )
-    current_time = datetime.now(tz=pytz.utc)
-    cron = croniter(schedule_expression, current_time)
-    response = cron.get_next(datetime).astimezone(pytz.utc)
-    cache.set(cache_key, response, 1800)
-    return response
+    try:
+        cache_key = NEXT_EXCUTION_TIME_CACHE.format(organization)
+        cache_response = cache.get(cache_key)
+        if cache_response and cache_response > datetime.now(tz=pytz.utc):
+            return cache_response
+        rule_name = settings.GETTING_NEW_ORDER_RULE_NAME
+        rule_details = client_events.describe_rule(Name=rule_name)
+        schedule_expression = rule_details["ScheduleExpression"]
+        schedule_expression = (
+            schedule_expression.replace("cron(", "").replace(")", "").replace("?", "")
+        )
+        current_time = datetime.now(tz=pytz.utc)
+        cron = croniter(schedule_expression, current_time)
+        response = cron.get_next(datetime).astimezone(pytz.utc)
+        cache.set(cache_key, response, 1800)
+        return response
+    except Exception:
+        return None
