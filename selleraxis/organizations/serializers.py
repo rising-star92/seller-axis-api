c7c80d8925dd5803c045729bd3708e1ecd24a640
rising-star92 <ryan.service.1001@gmail.com>
2023-12-20 15:12:57 +0700

    feat: update api update retrieve org

--- selleraxis/organizations/serializers.py
+++ selleraxis/organizations/serializers.py
@@ -42,21 +42,28 @@ class OrganizationSerializer(serializers.ModelSerializer):
 class UpdateOrganizationSerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=False)
     is_sandbox = serializers.BooleanField(required=False)
+    sandbox_organization = serializers.SerializerMethodField(read_only=True)
+
+    def get_sandbox_organization(self, organization):
+        sandbox_organization = organization.sandbox_organization
+        if sandbox_organization and sandbox_organization.sandbox_organization is None:
+            return OrganizationSerializer(sandbox_organization).data
+        return None
 
     class Meta:
         model = Organization
-        fields = "__all__"
         extra_kwargs = {
             "id": {"read_only": True},
             "created_by": {"read_only": True},
             "created_at": {"read_only": True},
             "updated_at": {"read_only": True},
             "deleted_at": {"read_only": True},
-            "realm_id": {"read_only": True},
-            "qbo_access_token": {"read_only": True},
-            "qbo_refresh_token": {"read_only": True},
-            "qbo_access_token_exp_time": {"read_only": True},
-            "qbo_user_uuid": {"read_only": True},
             "qbo_refresh_token_exp_time": {"read_only": True},
-            "sandbox_organization": {"read_only": True},
         }
+        exclude = [
+            "qbo_access_token",
+            "qbo_refresh_token",
+            "qbo_access_token_exp_time",
+            "qbo_user_uuid",
+            "realm_id",
+        ]
